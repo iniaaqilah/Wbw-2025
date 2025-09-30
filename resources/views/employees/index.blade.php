be3ee2f33874168428c6414b096185a72622de88
iniaaqilah <iniaaqilah@gmail.com>
2025-09-30 15:33:51 +0700

    Penambahan Master Layout

--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -1,8 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-    </head>
-    <body>
+@extends('master')
+@section('title', 'Daftar Pegawai')
+@section('content')
         <div class="container mt-5">
             <h1 class="mb-4">Daftar Pegawai </h1>
             <table border="1" cellpadding="5" cellspacing="0">
@@ -42,6 +40,5 @@
             </tbody>
         </table>
     </div>
-</body>
-</html>
+@endsection
 
